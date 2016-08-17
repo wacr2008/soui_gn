a0c01a000d4d33558fd96cde25284efa5756b53b
wacr2008 <24015500@qq.com>
2016-08-17 15:14:31 +0800

    add vm check

--- SOUI_gn/gen.bat
+++ SOUI_gn/gen.bat
@@ -3,7 +3,12 @@ call env.bat
 call python %~dp0source\config\com_gen.py shared_library 
 call python %~dp0source\config\core_gen.py shared_library 
 
-gn gen --ide=vs2013 out/Debug "--args=is_debug=true is_official_build=false is_component_build=true target_os=\"win\" target_cpu=\"x86\""
 
-gn gen --ide=vs2013 out/Release "--args=is_debug=false is_official_build=true is_component_build=true target_os=\"win\" target_cpu=\"x86\""
-
+if "%VM_BUILD%" == "" goto devBuild
+gn gen out/Debug "--args=is_debug=true use_system_yasm=true is_official_build=false is_component_build=true target_os=\"win\" target_cpu=\"x86\""
+gn gen out/Release "--args=is_debug=false use_system_yasm=true is_official_build=true is_component_build=true target_os=\"win\" target_cpu=\"x86\""
+goto end
+:devBuild
+gn gen --ide=vs2013 out/Debug "--args=is_debug=true use_system_yasm=true is_official_build=false is_component_build=true target_os=\"win\" target_cpu=\"x86\""
+gn gen --ide=vs2013 out/Release "--args=is_debug=false use_system_yasm=true is_official_build=true is_component_build=true target_os=\"win\" target_cpu=\"x86\""
+:end
--- SOUI_gn/gen_static.bat
+++ SOUI_gn/gen_static.bat
@@ -4,7 +4,12 @@ call python %~dp0source\config\com_gen.py static_library
 call python %~dp0source\config\core_gen.py static_library 
  
 
-gn gen --ide=vs2013 out/Debug "--args=is_debug=true is_official_build=false is_component_build=false target_os=\"win\" target_cpu=\"x86\""
 
-gn gen --ide=vs2013 out/Release "--args=is_debug=false is_official_build=true is_component_build=false target_os=\"win\" target_cpu=\"x86\""
-
+if "%VM_BUILD%" == "" goto devBuild
+gn gen out/Debug "--args=is_debug=true use_system_yasm=true is_official_build=false is_component_build=false target_os=\"win\" target_cpu=\"x86\""
+gn gen out/Release "--args=is_debug=false use_system_yasm=true is_official_build=true is_component_build=false target_os=\"win\" target_cpu=\"x86\""
+goto end
+:devBuild
+gn gen --ide=vs2013 out/Debug "--args=is_debug=true use_system_yasm=true is_official_build=false is_component_build=false target_os=\"win\" target_cpu=\"x86\""
+gn gen --ide=vs2013 out/Release "--args=is_debug=false use_system_yasm=true is_official_build=true is_component_build=false target_os=\"win\" target_cpu=\"x86\""
+:end
