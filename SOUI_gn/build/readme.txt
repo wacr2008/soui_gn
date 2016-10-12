b7031445777be793071e389ac71e03662c35902b
wacr2008 <24015500@qq.com>
2016-10-12 11:54:11 +0800

    同步最新的chrome build目录，以及最新gn程序，看到google官网也采用了与我类似的分开mt,md的方式了，因此去掉原来写的mdd mtd config，直接使用google官网的default_crt,dynamic_crt config了，方便以前同步

--- SOUI_gn/build/readme.txt
+++ SOUI_gn/build/readme.txt
@@ -1,15 +1,12 @@
 ﻿添加文件列表
-1.build\win_toolchain.json
-2.build\common.gni
+1.build\win_toolchain.json  支持不安装vs2013也可以编绎
+2.build\common.gni  加入safeseh开关
 
 修改文件列表
-
-
-1.build\config\BUILD.gn   //预编绎这里改了下，debug也改为预编了
-2.build\vs_toolchain.py  //修改json路径，可以不安装vs编绎
+1.build\config\BUILD.gn   //预编绎这里改了下，debug模式也改为使用预编了
+2.build\vs_toolchain.py  //修改json路径，可以不安装visual studio ide了
 3.build\config\win\BUILD.gn  修改默认的mt,md，改为通过指定config来设置
-4.build_overrides\build.gni 定义一些全局参数
-5.build\precompile.h 加了自己的预处理，这不用解释了吧
-6.build\toolchain\toolchain.gni 加入safeseh开关
+4.build\precompile.h 加了自己的预处理，这不用解释了吧
+5.build_overrides\build.gni 定义一些全局参数
 
-chrome代码日期：20160928
+chrome代码日期：20161011
--- SOUI_gn/gen_static.bat
+++ SOUI_gn/gen_static.bat
@@ -6,10 +6,10 @@ call python %~dp0src\config\core_gen.py static_library
 
 
 if "%VM_BUILD%" == "" goto devBuild
-gn gen out/Debug "--args=is_debug=true use_system_yasm=true is_official_build=false is_component_build=false target_os=\"win\" target_cpu=\"x86\""
-gn gen out/Release "--args=is_debug=false use_system_yasm=true is_official_build=true is_component_build=false target_os=\"win\" target_cpu=\"x86\""
+gn gen out/Debug "--args=is_debug=true  enable_safeseh=true use_system_yasm=true is_official_build=false is_component_build=false target_os=\"win\" target_cpu=\"x86\""
+gn gen out/Release "--args=is_debug=false  enable_safeseh=true use_system_yasm=true is_official_build=true is_component_build=false target_os=\"win\" target_cpu=\"x86\""
 goto end
 :devBuild
-gn gen --ide=vs2013 out/Debug "--args=is_debug=true use_system_yasm=true is_official_build=false is_component_build=false target_os=\"win\" target_cpu=\"x86\""
-gn gen --ide=vs2013 out/Release "--args=is_debug=false use_system_yasm=true is_official_build=true is_component_build=false target_os=\"win\" target_cpu=\"x86\""
+gn gen --ide=vs2013 out/Debug "--args=is_debug=true  enable_safeseh=true use_system_yasm=true is_official_build=false is_component_build=false target_os=\"win\" target_cpu=\"x86\""
+gn gen --ide=vs2013 out/Release "--args=is_debug=false  enable_safeseh=true use_system_yasm=true is_official_build=true is_component_build=false target_os=\"win\" target_cpu=\"x86\""
 :end
