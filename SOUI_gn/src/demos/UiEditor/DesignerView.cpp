9375c4a41f76e8e8ae9a2770adb15d539c9e57e0
wacr2008 <24015500@qq.com>
2016-11-08 16:36:28 +0800

    fix protobuf fatal error C1091: compiler limit: string exceeds 65535 bytes in length,重新修改protobuf2.3的代码，修正这个问题，并上了二进制文件，新的protobuf已修正了这个问题了

--- SOUI_gn/src/demos/UiEditor/DesignerView.cpp
+++ SOUI_gn/src/demos/UiEditor/DesignerView.cpp
@@ -2021,7 +2021,8 @@ void SDesignerView::InitXMLStruct(pugi::xml_node xmlNode, HSTREEITEM item)
 		SStringT strNodeName = NodeSib.name();
 		if (strNodeName.IsEmpty())
 		{
-			return;
+			NodeSib = NodeSib.next_sibling();
+			continue;
 		}
 
 		HSTREEITEM itemChild = m_treeXmlStruct->InsertItem(strNodeName, item);  
